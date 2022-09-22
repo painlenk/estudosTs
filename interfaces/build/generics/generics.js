1f7365d501c06057598bb1879e4c6742a04fd52e
leonardo valle <leonardo.valle@eduzz.com>
2022-09-22 14:30:30 -0300

    testes com generics

--- interfaces/build/generics/generics.js
+++ interfaces/build/generics/generics.js
@@ -1,5 +1,5 @@
 "use strict";
-function Data() {
+function data() {
     let dados;
     function getData() {
         return dados;
@@ -7,4 +7,11 @@ function Data() {
     function setData(item) {
         dados = item;
     }
+    return { getData, setData };
 }
+const dataNumber = data();
+dataNumber.setData(15);
+console.log(dataNumber.getData());
+const dataString = data();
+dataString.setData("texto");
+console.log(dataString.getData());
--- interfaces/src/generics/generics.ts
+++ interfaces/src/generics/generics.ts
@@ -1,11 +1,21 @@
-function Data () {
-    let dados: <T>;
+function data<T>() {
+  let dados: T;
 
-    function getData() {
-        return dados
-    }
+  function getData() {
+    return dados;
+  }
 
-    function setData(item: <T>) {
-        dados = item
-    }
+  function setData(item: T) {
+    dados = item;
+  }
+
+  return { getData, setData };
 }
+
+const dataNumber = data<number>();
+dataNumber.setData(15);
+console.log(dataNumber.getData());
+
+const dataString = data<String>();
+dataString.setData("texto");
+console.log(dataString.getData());
